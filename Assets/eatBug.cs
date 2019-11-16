e8670f79486fd532d82d1db072b95f3f0e75b964
DALI Lab <dali@dali.dartmouth.edu>
2019-11-16 16:06:26 -0500

    Andy save

---
+++ Assets/eatBug.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class eatBug : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnEatBug(Collision col)
+    {
+        if (col.gameObject.name == "simple_katydid_tree")
+        {
+            Debug.Log("Collision Detected");
+        }
+    }
+}
